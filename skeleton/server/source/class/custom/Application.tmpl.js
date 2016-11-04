f5ceda2235816bb76b9217e663632bb24defd8f7
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-11-04 16:41:56 -0400

    Update static path.

--- skeleton/server/source/class/custom/Application.tmpl.js
+++ skeleton/server/source/class/custom/Application.tmpl.js
@@ -26,22 +26,28 @@ qx.Class.define("${Namespace}.Application", {
 
             if (this.itIsProduction()) {
                 qx.lang.Array.append(paths, [
-                    { urlPattern: '/gui', resourcePath: guaraiba.appRoot },
-                    { urlPattern: '/gui/qx', resourcePath: path.join(guaraiba.appRoot, 'resource/qx') },
-                    { urlPattern: '/gui/polymita', resourcePath: path.join(guaraiba.appRoot, 'resource/polymita') },
+                    {urlPattern: '/gui', resourcePath: guaraiba.appRoot},
+                    {urlPattern: '/gui/qx', resourcePath: path.join(guaraiba.appRoot, 'resource', 'qx')},
+                    {urlPattern: '/gui/polymita', resourcePath: path.join(guaraiba.appRoot, 'resource', 'polymita')},
                 ]);
             } else {
                 var module = require('module'),
-                    guiSourcePath = path.resolve(path.join(guaraiba.appRoot, '../../${Name}-gui/source')),
                     polymitaPath = path.dirname(module._resolveFilename('polymita')),
-                    polymitaGuiResourcePath = path.join(polymitaPath, 'node_modules/polymita-gui/source/resource/polymita'),
-                    qxSourcePath = path.join(polymitaPath, 'node_modules/qooxdoo-sdk/framework/source/resource/qx');
+                    guiSourcePath = path.resolve(path.join(guaraiba.appRoot,
+                        '..', '..', '${Name}-gui', 'source')
+                    ),
+                    polymitaGuiResourcePath = path.join(polymitaPath,
+                        'node_modules', 'polymita-gui', 'source', 'resource', 'polymita'
+                    ),
+                    qxSourcePath = path.join(polymitaPath,
+                        'node_modules', 'qooxdoo-sdk', 'framework', 'source', 'resource', 'qx'
+                    );
 
                 qx.lang.Array.append(paths, [
-                    { urlPattern: '/gui', resourcePath: guiSourcePath },
-                    { urlPattern: '/gui/qx', resourcePath: qxSourcePath },
-                    { urlPattern: '/gui/polymita', resourcePath: polymitaGuiResourcePath },
-                    { urlPattern: '/source', resourcePath: guiSourcePath }
+                    {urlPattern: '/gui', resourcePath: guiSourcePath},
+                    {urlPattern: '/gui/qx', resourcePath: qxSourcePath},
+                    {urlPattern: '/gui/polymita', resourcePath: polymitaGuiResourcePath},
+                    {urlPattern: '/source', resourcePath: guiSourcePath}
                 ]);
             }
 
